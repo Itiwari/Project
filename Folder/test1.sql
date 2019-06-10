3289a7c60f151edfb1548d225b13defad86b1479
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-10 12:46:49 +0530

    Create test1.sql

---
+++ Folder/test1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test1 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO test1 VALUES ( 1, 'Alpha' );
+INSERT INTO test2 VALUES ( 2, 'Beta' );
+INSERT INTO test2 VALUES ( 3, 'Gamma' );
+INSERT INTO test2 VALUES ( 4, 'Ele' ); 
